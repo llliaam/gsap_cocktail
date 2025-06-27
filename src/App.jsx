e63d2818a5739014744e2590701a940f72aaf76f
llliaam <benedigw@gmail.com>
2025-06-27 15:00:32 +0700

    Finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import Art from './components/Art';
+import About from './components/About';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+
 
 gsap.registerPlugin( ScrollTrigger, SplitText );
 
@@ -13,6 +18,10 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Cocktails/>
+      <About/>
+      <Art/>
+      <Menu/>
+      <Contact/>
     </main>
   )
 }
