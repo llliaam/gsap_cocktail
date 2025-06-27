e5fc8c3c42fa7a3e94c62c7ce7a27173b2470cd0
llliaam <benedigw@gmail.com>
2025-06-27 14:00:15 +0700

    Animasi teks hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,80 @@
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import gsap from 'gsap'
+import React from 'react'
+
+const Hero = () => {
+
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', { type: 'chars, words'});
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+        .to('.right-leaf', { y: 200}, 0)
+        .to('.left-leaf', { y: -200}, 0)
+    }, [])
+  return (
+    <>
+        <section id='hero' className='noisy'>
+            <h1 className='title'>LIAM</h1>
+
+            <img 
+                src="/images/hero-left-leaf.png" 
+                alt="left-leaf" 
+                className='left-leaf'
+            />
+
+            <img 
+                src="/images/hero-right-leaf.png" 
+                alt="right-leaf" 
+                className='right-leaf'
+            />
+
+            <div className='body'>
+                <div className='content'>
+                    <div className='space-y-5 hidden md:block'>
+                        <p>IT. Student. Programmer.</p>
+                        <p className='subtitle'>
+                            I'm an Undergraduate <br /> IT Student
+                        </p>
+                    </div>
+                    <div className='view-cocktails'>
+                        <p className='subtitle'>
+                            I'm William Benediktus an IT student from Universitas Sumatera Utara that have passion in
+                            web development, design and Cybersecurity.
+                        </p>
+                        <a href="#cocktails">View this web!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Hero
